88fb07b251c72d45977af94eb4e654fa60edd3a6
Thrisha <msthrisha@gmail.com>
2022-04-26 22:10:36 +0530

    added the file

---
+++ cow.py
@@ -0,0 +1,45 @@
+from csv import writer
+
+out = writer(open("cow.csv", "a"))         #creating output file w mode always creat a new file
+record = [] #empty record for single record
+records = [] #empty record for multiple record
+
+#Reading Cow detail start
+flag = 'y'
+
+while flag.lower() == 'y'.lower():
+    cow_id = input('Cow id? ')
+    breed = input('Breed? ')
+    dob = input('Bob (dd/mm/yy)? ')
+    weight = input('Weight? ')
+    color = input('Color? ')
+    healthstatus = input('Health status? ')
+    gender = input('Gender (Male/Female)? ')
+    if (gender.upper() == "female".upper() or gender.upper() == "f".upper()):
+        milking = input('Milking (y/n)? ')
+        gestationperiod = input('Gestationperiod (y/n)? ')
+
+    #milking = input('Milking (y/n)? ')
+    vaccinationschedule = input('Vaccination schedule (dd/mm/yy) ')
+    #Reading Cow detail end
+
+    #adding Cow detail into record start
+    record.append(cow_id) 
+    record.append(breed) 
+    record.append(dob) 
+    record.append(weight) 
+    record.append(color) 
+    record.append(gender) 
+    if ((gender.upper() == "female".upper()) or (gender.upper() == 'f'.upper())):
+        record.append(milking) 
+        record.append(gestationperiod) 
+    record.append(healthstatus) 
+    record.append(vaccinationschedule) 
+    
+    records.append(record) # adding to recored
+    #adding Cow detail into record end
+    flag = input('Continue (y/n)? ')
+    record = [] # empty record object
+
+for record in records: # write one by one using for loop
+    out.writerow(record) # writing to output file
