71bc5fcc099360adb9a7c1815933b6b2f148dc14
PythonTrainingSP <pythontraning737@email.com>
2022-05-03 14:05:39 +0530

    modified expense

---
+++ expensedetail.py
@@ -0,0 +1,23 @@
+from dbconnection import *
+
+record = {} #empty record for single record should be dic/JSON Object
+records = [] #empty record for multiple record
+
+flag="y"
+while flag=="y" or flag=="Y":
+    transaction_date = input('Transcation Date ')
+    type_expense = input('Expense Type (Worker/Food/Doctor/Maintenance) ')
+    amount = float(input('Amount (Rs) '))
+
+    record["transaction_date"] = transaction_date
+    record["type_expense"] = type_expense
+    record["amount"] = amount
+    
+    records.append(record)
+    flag=input("continue(y/n)" )
+    record = {}
+    
+#print(record) #user details
+for record in records:
+    spexpense.insert_one(record)
+
