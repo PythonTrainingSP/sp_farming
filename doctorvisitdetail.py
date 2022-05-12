250cf002268a8c1f2c996b78946898768b3382d8
PythonTrainingSP <pythontraning737@gmail.com>
2022-05-12 22:48:13 +0530

    modify

--- doctorvisitdetail.py
+++ doctorvisitdetail.py
@@ -1,6 +1,43 @@
-#this program created for addind doctor visit
+'''
+Author: vijaya bharathi
+Date: 12/05/2022
+
+version 1.0
+converted csv writing to mongodb
+version 1.1
+'''
 from dbconnection import *
 
+#only for writing DB
+def readdoctordetailbydate (date):
+    Doctordetail = spdoctorvisit.find({"date":date})
+    return Doctordetail
+
+def readdoctordetailbyname (name):
+    Doctordetail = spdoctorvisit.find({"name":name})
+    return Doctordetail
+
+def readdoctordetailbycowid(Cow_id):
+    Doctordetail = spdoctorvisit.find({"Cow_id":Cow_id})
+    return Doctordetail
+
+def readdoctordetailbyvaccinename(VaccineName):
+    Doctordetail = spdoctorvisit.find({"VaccineName":VaccineName})
+    return Doctordetail
+
+def readdoctordetailbyvaccinetype(VaccineType):
+    Doctordetail = spdoctorvisit.find({"VaccineType":VaccineType})
+    return Doctordetail
+
+def readdoctordetailbydose(dose):
+    Doctordetail = spdoctorvisit.find({"dose":dose})
+    return Doctordetail
+
+def readdoctordetailbyfee(fee):
+    Doctordetail = spdoctorvisit.find({"fee":fee})
+    return Doctordetail
+
+'''
 record = {}  # empty record for single record
 records = []  # empty record for multiple record
 
@@ -31,6 +68,7 @@ while flag == "Y" or flag == "y":
     # adding doctor detail into record end
      flag = input('Continue (y/n)? ')
      record = {}         #empty record object
+'''
 
 for record in records:
     spdoctorvisit.insert_one(record)
