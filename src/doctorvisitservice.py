f4e53a30b1a9f3dedc4516ee73fb81d0175d392b
PythonTrainingSP <pythontraning737@gmail.com>
2022-05-22 18:58:57 +0530

    command line

---
+++ src/doctorvisitservice.py
@@ -0,0 +1,25 @@
+from doctorvisitdetail import *
+from bson.json_util import dumps, loads
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/doctorvisitdetaillist') # http://localhost:5400/feeddetail
+# ‘/’ URL is bound with hello_world() function.
+def getdoctorvisitDetailList():
+    record = readdoctorvisitdetaillist()  #for reading feeddetail list
+    list_cur = list(record)
+    # Converting to the JSON
+    json_data = dumps(list_cur, indent = 2) 
+    return json_data
+
+    @app.route('/doctorvisitdetail/<date>')
+
+def getdocotorvisitDetail(date):
+    record = readdoctorvisitdetailbydate(date)
+    list_cur = list(record)
+
+    json_data = dumps(list_cur, indent = 2) 
+    return json_data
+
+    if __name__ == '__main__':
+         app.run()
