0047dc003131718cdffe2cfdaba5c928e2d3624c
PythonTrainingSP <pythontraning737@email.com>
2022-05-25 22:47:03 +0530

    modified curd(add,update,get)

--- src/feeddetail.py
+++ src/feeddetail.py
@@ -25,7 +25,7 @@ def readfeeddetailbytime(time):
     return feeddetail
 
 def readfeeddetailbytypesoffeed(types_of_feed):
-    feeddetail =spfeed.find({"types of feed":types_of_feed})
+    feeddetail =spfeed.find({"types_of_feed":types_of_feed})
     return feeddetail
 
 def readfeeddetailbyquantity(quantity):
@@ -37,13 +37,18 @@ def readfeeddetailbywatering(watering):
     return feeddetail
 
 def readfeeddetailbyotherminerals(other_minerals):
-    feeddetail = spfeed.find({"other minerals":other_minerals})
+    feeddetail = spfeed.find({"other_minerals":other_minerals})
     return feeddetail
+
+def addfeeddetail(date,time,types_of_feed,quantity,watering,other_minerals):
+    spfeed.insert_one({"date":date, "time":time, "types_of_feed":types_of_feed, 
+    "quantity":quantity, "watering":watering, "other_minerals":other_minerals})
 
 #modify a single data
 def updatefeeddetailbydate(date, time, types_of_feed, quantity, watering, other_minerals):
-    spfeed.update_one({"date":date}, {"$set": {"time":time, "typesoffeed":types_of_feed, "quantity":quantity, "watering":watering, "otherminerals":other_minerals}})
+    spfeed.update_one({"date":date}, {"$set": {"time":time, "types_of_feed":types_of_feed, 
+    "quantity":quantity, "watering":watering, "other_minerals":other_minerals}})
 
 #remove a specific data
-def removefeeddetailbydate(date):
+def deletefeeddetailbydate(date):
     spfeed.delete_one({"date":"03/05/2022"})
